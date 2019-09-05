63be9c31f0d520672cb9aca6dab02212e7770db7
zhashangjun <csharpgongzi@163.com>
2019-09-05 15:50:53 +0800

    递归查找指定对象组的所有丢失组件

---
+++ Utility/Editor/FindMissingScriptsRecursively.cs
@@ -0,0 +1,68 @@
+﻿using UnityEngine;
+using UnityEditor;
+/// <summary>
+/// 查找指定对象组的所有丢失组件
+/// </summary>
+public class FindMissingScriptsRecursively : EditorWindow
+{
+    static int go_count = 0, components_count = 0, missing_count = 0;
+
+    [MenuItem("Window/FindMissingScriptsRecursively")]
+    public static void ShowWindow()
+    {
+        EditorWindow.GetWindow(typeof(FindMissingScriptsRecursively));
+    }
+
+    public void OnGUI()
+    {
+        if (GUILayout.Button("Find Missing Scripts in selected GameObjects"))
+        {
+            FindInSelected();
+        }
+    }
+    private static void FindInSelected()
+    {
+        GameObject[] go = Selection.gameObjects;    //获取选择的多个对象数组
+        go_count = 0;
+        components_count = 0;
+        missing_count = 0;
+        foreach (GameObject g in go)
+        {
+            FindInGO(g);
+        }
+        Debug.Log(string.Format("Searched {0} GameObjects, {1} components, found {2} missing", go_count, components_count, missing_count));
+    }
+
+    /// <summary>
+    /// 递归查找每个对象及其所有子对象的丢失组件
+    /// </summary>
+    /// <param name="g">指定的游戏对象</param>
+    private static void FindInGO(GameObject g)
+    {
+        go_count++;
+        Component[] components = g.GetComponents<Component>();
+        for (int i = 0; i < components.Length; i++)
+        {
+            components_count++;
+            if (components[i] == null)
+            {
+                missing_count++;
+                string s = g.name;
+                Transform t = g.transform;
+                while (t.parent != null)
+                {
+                    s = t.parent.name + "/" + s;
+                    t = t.parent;
+                }
+                Debug.Log(s + " has an empty script attached in position: " + i, g);
+            }
+        }
+        // Now recurse through each child GO (if there are any):
+        // 遍历子对象并递归调用
+        foreach (Transform childT in g.transform)
+        {
+            //Debug.Log("Searching " + childT.name  + " " );
+            FindInGO(childT.gameObject);
+        }
+    }
+}
